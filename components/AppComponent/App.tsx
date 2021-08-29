353b890c2ab3f10f547f762c1cdd64db147caea3
edenk <edenk@wix.com>
2021-08-29 12:21:51 +0300

    First commit

---
+++ components/AppComponent/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      <Text>Hello, Eden hello world hello what is going on?!!</Text>
+    </View>
+  );
+};
+export default App;
